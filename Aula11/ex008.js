c5a7ecd4b41a002b8cb8506f34cfd21ad7ee3df5
codestreambr <carandre@codestream.com.br>
2023-03-31 11:06:58 -0300

    Files update

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 79.9;
+console.log(`A velocidade atual do seu carro é: ${vel}Km/h`);
+if (vel > 80) {
+    console.log('Você foi multado!');
+}
+console.log('Pode seguir sua viagem.');
---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brazil';
+
+console.log(`Sua residência atual é: ${pais}`);
+if (pais == 'Brasil' || pais =='Brazil') {
+  console.log('Você é Brasileiro');
+} else {
+  console.log('Você é Estrangeiro');
+}
