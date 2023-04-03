a7e36f48efa338ce615983b811729b5b71bc1a13
codestreambr <carandre@codestream.com.br>
2023-04-03 07:57:41 -0300

    Update exercices 12 and 13

---
+++ Aula11/ex013.js
@@ -0,0 +1,30 @@
+var dia = new Date();
+
+var diaSemana = 5;
+
+console.log(`O dia da semana é ${diaSemana}`);
+
+switch (diaSemana) {
+  case 0:
+    console.log('Domingo');
+    case 1:
+    console.log('Segunda');
+    break;
+    case 2:
+    console.log('Terça');
+    break;
+    case 3:
+    console.log('Quarta');
+    break;
+    case 4:
+    console.log('Quinta');
+    break;
+    case 5:
+    console.log('Sexta');
+    break;
+    case 6:
+    console.log('Sábado');
+    //break;
+    default:
+      break;
+}
