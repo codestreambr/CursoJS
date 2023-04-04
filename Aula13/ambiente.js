3beca40b5b7d586e03315a292f134436f06751ee
codestreambr <carandre@codestream.com.br>
2023-04-04 05:38:20 -0300

    Update exe013

---
+++ Aula13/ambiente.js
@@ -0,0 +1,13 @@
+console.log('Teste lógico no início');
+d = 0;
+while (d <= 4) {
+    console.log(`Loop: ${d}`)
+    d++;
+}
+
+console.log('Teste lógico no final');
+d = 1;
+do {
+    console.log(`Loop: ${d}`)
+    d++;
+} while (d <= 4);
